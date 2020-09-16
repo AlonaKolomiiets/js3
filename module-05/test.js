2625af43e5371c8304ee4bf6f94bbb7cb9f6e13a
Алена Коломиец <alenakolomiec@MacBook-Air-Alena.local>
2020-09-16 22:34:46 +0300

    added additional task

--- module-05/test.js
+++ module-05/test.js
@@ -241,22 +241,22 @@
 // newFn.getTwoNumbers();
 // newFn.newFunctionality(Math.pow);
 
-class Cats {
-  constructor(
-    whiskers = "default",
-    teeth = "default",
-    tail = "default",
-    claws = "default"
-  ) {
-    this.whiskers = whiskers;
-    this.teeth = teeth;
-    this.tail = tail;
-    this.claws = claws;
-  }
-  static catsInfo() {
-    console.log(`Every one in Cats class has mrrrr-engine`);
-  }
-}
+// class Cats {
+//   constructor(
+//     whiskers = "default",
+//     teeth = "default",
+//     tail = "default",
+//     claws = "default"
+//   ) {
+//     this.whiskers = whiskers;
+//     this.teeth = teeth;
+//     this.tail = tail;
+//     this.claws = claws;
+//   }
+//   static catsInfo() {
+//     console.log(`Every one in Cats class has mrrrr-engine`);
+//   }
+// }
 // class Lions extends Cats {
 //   constructor(whiskers, teeth, tail, claws, mane = "default") {
 //     super(whiskers, teeth, tail, claws);
@@ -319,3 +319,67 @@ class Cats {
 // let hero1 = new Hero(boris);
 // console.log(hero1);
 
+// // == task-2 == //
+// Створити класи Library та Book
+
+// 1. Клас Library буде описувати нашу бібліотеку книжок. Всі книжки будуть зберігатися в масиві.
+// Також цей клас буде мати такі методи:
+
+// - Метод buy - це метод що приймає аргументом обєкт книги і додає її в бібліотеку якщо її там ще немає.
+//Якщо книга вже є показує повідомлення що книга вже куплена
+// - Метод sell - це метод що приймає аргументом назву книги і видаляє її з бібліотеки
+// - Метод addToFavourite - це метод що приймає аргументом назву книги і додає обєкт книги в список улюблених
+// - Метод removeFromFavourite - це метод що приймає назву книги і видаляє її з списку улюблених
+// - Гетер сountFavouriteBooks - це гетер що показує кількість книжок що додані в улюблені
+// - Гетер finishedBook - це гетер що містить колекцію всіх прочитаних книжок
+// - Метод totalCost - це метод що рахує вартість всіх книжок в бібліотеці
+
+class Library {
+  constructor() {}
+}
+// 2. Клас Book буде описувати окрему книгу. Кожна книга це буде обєкт з такими ключами
+// - author
+// - title
+// - price
+// - totalPages
+// - currentPage
+
+// Також цей клас буде мати такі методи:
+
+// - Метод read - це метод що приймає кількість сторінок що було прочитано і міняє значення currentPage
+// - Гетер bookProgres - гетер що переводить кількість прочитаних сторінок в відсотки
+// - Сетер bookProgres - сетер що переводить відсоток прочитаних сторінок в кількість
+// Клас Book приймає один аргумент - обєкт з парметрами книги. В середині класу використовувати
+//деструктуризацію обєкта
+
+class Book {
+  constructor({ author, title, price, totalPages, currentPage = 0 }) {
+    (this.author = author),
+      (this.title = title),
+      (this.price = price),
+      (this.totalPages = totalPages),
+      (this.currentPage = currentPage);
+  }
+  read(numberOfpagesWasRead) {
+    this.currentPage = numberOfpagesWasRead;
+  }
+  get bookProgres() {
+    return (this.currentPage * 100) / this.totalPages;
+  }
+  set bookProgres(percent) {
+    this.currentPage = (this.totalPages * percent) / 100;
+  }
+}
+
+const myBook = new Book({
+  author: "Tolstoy",
+  title: "War and Peace",
+  price: 300,
+  totalPages: 500,
+  currentPage: 0,
+});
+
+myBook.read(250);
+console.log(myBook.bookProgres);
+myBook.bookProgres = 1;
+console.log(myBook.bookProgres);
